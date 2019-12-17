45d272c9ddabfc374f04f6e95f1e1cf9eaa733a6
zhuchong <1043199504@qq.com>
2019-12-17 10:39:38 +0800

    华擎身份证阅读器Java-web接口

---
+++ src/main/java/com/zhu/demo/Cvr100Application.java
@@ -0,0 +1,13 @@
+package com.zhu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvr100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvr100Application.class, args);
+    }
+
+}
